656a357e7d9f747fd33d655d00308e4b1ebab7bb
Jerome D Nelson <jerome.nelson@netplaytv.com>
2015-10-20 18:41:39 +0100

    - All positions added. Also added check for padding and margin border.

--- center.jq.js
+++ center.jq.js
@@ -6,60 +6,72 @@
 (function($) {
     $.fn.centered = function(options) {
 
-        var settings,coords,posalign,getpos,finalpos,elemdim;
+        var settings,coords,posalign,getpos,finalpos,elemcoords,totwidth,totheight;
 
         settings = $.extend({
 
             //Element to center (can contain anything)
             //Find way to refer to element without using hard string
             elem: $('.loading'),
-            position: 'topLeft',
-            offset: '',
-            offset2: '',
+            position: 'bottom',
+            offsetX: '', //horizontal movement
+            offsetY: '', //Vertical Movement
+            offsetScope: $(this),//Default is targeted element but will take any other selector
             callback: ''
         }, options);
 
+
         //Positioning Alignment Array
         //TODO: use existing js positioning properties if exist
-        posalign = ['top','topLeft','topRight','topCenter','center','centerLeft','centerRight','centerCenter','bottom','bottomLeft','bottomRight','bottomCenter'];
+        posalign = ['top','topLeft','topRight','topCenter','center','centerLeft','centerRight','bottom','bottomLeft','bottomRight','bottomCenter'];
 
-        //We get the Prototype of Jquery selector with [0]
-        //NOTE: Unless this is put in document.ready() will return error
-        coords = $(this)[0].getBoundingClientRect(); //Returns Prototype
+        //We get the Prototype of Jquery selector and elem with [0]
+        coords = $(this)[0].getBoundingClientRect();
+        elemcoords = settings.elem[0].getBoundingClientRect();
 
-       // console.log(coords);
-
-        elemdim = settings.elem[0].getBoundingClientRect();
-
-        //Get key (array position) for position setting
-        getpos = posalign.lastIndexOf(settings.position);
-
+        //Array containing all positioning co-ords for selector. We manipulate to get final results
         finalpos = [coords.top,coords.bottom,coords.left,coords.right];
 
+        //If outerHeight matches current height and width then there is no extra padding,borders,margins to worry about
+        if(parseInt(coords.height,10) !== $(this).outerHeight(true)) {totheight = ($(this).outerHeight(true) - parseInt(coords.height,10))/2;}
+        else { totheight = '0';}
+
+        //Now same for outerWidth
+        if(parseInt(coords.width,10) !== $(this).outerWidth(true)) { totwidth = ($(this).outerWidth(true) - parseInt(coords.width,10))/2;}
+        else { totwidth = '0';}
 
         //TODO: Group attributes better to remove DRY violation
         //TODO: (maybe use case's: multiple times in switch statement) - TRIED (WORKS IN CHROME BUT NOT VALID)
+
+        //Get key (array position) for position setting (we use for switch)
+        getpos = posalign.lastIndexOf(settings.position);
+
         if(!isNaN(getpos)) {
             switch(settings.position) {
 
-                //Take top and topCenter
+                //Take top, topCenter
                 case posalign[0]:
                 case posalign[3]:
                 default:
 
                     //left px formula
-                    finalpos[2] = (coords.width/2 - elemdim.width/2);
-
+                    finalpos[2] = (coords.width/2 - elemcoords.width/2);
                     //Top px formula
-                    finalpos[0] = coords.top-elemdim.top;
-
+                    finalpos[0] = coords.top;
                     //Remove right positioning
                     finalpos[3] = 'auto';
+                    break;
 
+                //Takes center
+                case posalign[4]:
+                    finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
+                    //left px formula
+                    finalpos[2] = (coords.width/2 - elemcoords.width/2);
                     break;
 
                 //Takes topLeft
                 case posalign[1]:
+
                     //Remove right positioning
                     finalpos[3] = 'auto';
                     break;
@@ -67,29 +79,76 @@
                 //Takes topRight
                 case posalign[2]:
 
-                    finalpos[2] = finalpos[3] - (elemdim.width);
+                    finalpos[2] = finalpos[3] - (elemcoords.width);
                     finalpos[3] = 'auto';
                     break;
 
-                //Takes center
+                //Takes centerLeft (center is at top)
                 case posalign[5]:
 
-
-
+                    //Top px formula
+                    finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
+                    //Remove right positioning
+                    finalpos[3] = 'auto';
                     break;
 
+                //Takes centerRight
+                case posalign[6]:
+
+                    //Top px formula
+                    finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
+                    finalpos[2] = finalpos[3] - (elemcoords.width);
+                    finalpos[3] = 'auto';
+                    break;
+
+                //Takes bottom
+                case posalign[7]:
+
+                    //left px formula
+                    finalpos[2] = (coords.width/2 - elemcoords.width/2);
+                    finalpos[0] = coords.bottom-elemcoords.height;
+                    finalpos[3] = 'auto';
+                    finalpos[1] = 'auto';
+                    break;
+
+                //Takes bottomLeft
+                case posalign[8]:
+
+                    finalpos[0] = coords.bottom-elemcoords.height;
+                    finalpos[3] = 'auto';
+                    finalpos[1] = 'auto';
+                    break;
+
+                //Takes bottomRight
+                case posalign[9]:
+                    //Top px formula
+                    finalpos[0] = coords.bottom-elemcoords.height;
+                    finalpos[2] = finalpos[3] - (elemcoords.width);
+                    break;
+
+                //Takes bottomCenter
+                case posalign[10]:
+
+                    //Top px formula
+                    finalpos[0] = coords.bottom-elemcoords.height;
+                    //left px formula
+                    finalpos[2] = (coords.width/2 - elemcoords.width/2);
+                    finalpos[3] = 'auto';
+                    break;
             }
         }
 
 
 
         //We use this to centerally position var elem onto var targetelem
-        return settings.elem.css({
-            position: 'absolute',
-            top: finalpos[0],
-            left: finalpos[2],
-            right: finalpos[3],
-            bottom: finalpos[1]
+        return this.each(function() {
+            settings.elem.css({
+                position: 'absolute',
+                top: finalpos[0],
+                left: finalpos[2],
+                right: finalpos[3],
+                bottom: finalpos[1]
+            });
         });
     };
 }(jQuery));
