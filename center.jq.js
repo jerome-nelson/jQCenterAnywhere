cf63f960cd095b05e7ef1c4261daebfc52bb1c7a
Jerome D Nelson <jerome.nelson@netplaytv.com>
2015-10-23 18:13:02 +0100

    - Currently applying callback

--- center.jq.js
+++ center.jq.js
@@ -5,11 +5,19 @@
 (function($) {
     $.fn.centered = function(options) {
 
-        var settings,coords,posalign,getpos,finalpos,elemcoords,totwidth,totheight,num;
+        var settings,coords,elemcoords,keywords,currentpos,finalpos,totwidth,totheight,func_list,calling;
         settings = $.extend({}, $.fn.centered.defaults, options);
 
+        //$this.on(event, function(
+        // akldfj;ajklsd
+        // ))
+
+        if (typeof settings.onStart === "function") {
+            settings.onStart.call(this);
+        }
+
         //TODO: Remove dependency on this (find assoc js function or native CS attribute names)
-        posalign = ['top','topLeft','topRight','topCenter','center','centerLeft','centerRight','bottom','bottomLeft','bottomRight','bottomCenter'];
+        keywords = ['top','topLeft','topRight','topCenter','center','centerLeft','centerRight','bottom','bottomLeft','bottomRight','bottomCenter'];
 
         //We get the Prototype of Jquery selector and elem with [0]
         coords = $(this)[0].getBoundingClientRect();
@@ -24,14 +32,14 @@
 
         //TODO: Group attributes better to remove DRY violation
         //Get key (array position) for position setting (we use for switch)
-        getpos = posalign.lastIndexOf(settings.position);
+        currentpos = keywords.lastIndexOf(settings.position);
 
-        if(!isNaN(getpos)) {
+        if(!isNaN(currentpos)) {
             switch(settings.position) {
 
                 //Take top, topCenter
-                case posalign[0]:
-                case posalign[3]:
+                case keywords[0]:
+                case keywords[3]:
                 default:
 
                     //left px formula
@@ -43,28 +51,28 @@
                     break;
 
                 //Takes center
-                case posalign[4]:
+                case keywords[4]:
                     finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
                     //left px formula
                     finalpos[2] = (coords.width/2 - elemcoords.width/2);
                     break;
 
                 //Takes topLeft
-                case posalign[1]:
+                case keywords[1]:
 
                     //Remove right positioning
                     finalpos[3] = 'auto';
                     break;
 
                 //Takes topRight
-                case posalign[2]:
+                case keywords[2]:
 
                     finalpos[2] = finalpos[3] - (elemcoords.width);
                     finalpos[3] = 'auto';
                     break;
 
                 //Takes centerLeft (center is at top)
-                case posalign[5]:
+                case keywords[5]:
 
                     //Top px formula
                     finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
@@ -73,7 +81,7 @@
                     break;
 
                 //Takes centerRight
-                case posalign[6]:
+                case keywords[6]:
 
                     //Top px formula
                     finalpos[0] = (totheight+(coords.height-elemcoords.height)/2);
@@ -82,7 +90,7 @@
                     break;
 
                 //Takes bottom
-                case posalign[7]:
+                case keywords[7]:
 
                     //left px formula
                     finalpos[2] = (coords.width/2 - elemcoords.width/2);
@@ -92,7 +100,7 @@
                     break;
 
                 //Takes bottomLeft
-                case posalign[8]:
+                case keywords[8]:
 
                     finalpos[0] = coords.bottom-elemcoords.height;
                     finalpos[3] = 'auto';
@@ -100,14 +108,14 @@
                     break;
 
                 //Takes bottomRight
-                case posalign[9]:
+                case keywords[9]:
                     //Top px formula
                     finalpos[0] = coords.bottom-elemcoords.height;
                     finalpos[2] = finalpos[3] - (elemcoords.width);
                     break;
 
                 //Takes bottomCenter
-                case posalign[10]:
+                case keywords[10]:
 
                     //Top px formula
                     finalpos[0] = coords.bottom-elemcoords.height;
@@ -117,10 +125,8 @@
                     break;
             }
         }
-
-
         //We use this to centerally position var elem onto var targetelem
-        return this.each(function() {
+        this.each(function() {
             settings.elem.css({
                 position: 'absolute',
                 top:   $.fn.centered.offsetCalc(settings.offsetX,finalpos[0]),
@@ -128,7 +134,14 @@
                 right: finalpos[3],
                 bottom: finalpos[1]
             });
+
         });
+
+
+        // other function code here
+        if (typeof settings.onComplete === "function") {
+            settings.onComplete();
+        }
     };
 
     //Allows users to access options as needed (probably not needed just testing)
@@ -138,9 +151,9 @@
         offsetX: '', //horizontal movement
         offsetY: '', //Vertical Movement
         //TODO: Once code is tidier (offsetScope - allowing person to pick element to base offset calcs off).
-        triggerOn: function() {}, //When to trigger movement
-        callback: function() {} //What to trigger when movement is done
-
+        //triggerOn: function() {}, //When to trigger movement
+        onStart: jQuery.noop,
+        onComplete: jQuery.noop
     };
 
 
@@ -192,5 +205,18 @@
         }
         
         return calc;
-    }
+    };
+
+    $.fn.centered.start = function() {
+
+    };
 }(jQuery));
+
+
+function test() {
+ return alert('wewerwe');
+}
+
+function test1() {
+    return alert('231231232');
+}
